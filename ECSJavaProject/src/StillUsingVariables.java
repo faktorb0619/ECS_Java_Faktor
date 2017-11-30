8e383f87be818c6cbe1e69d1fa3e4a85d0e3b648
benjaminfaktor <FaktorB0619@sduhsd.lan>
2017-11-30 10:35:10 -0800

    Sub.1

---
+++ ECSJavaProject/src/StillUsingVariables.java
@@ -0,0 +1,28 @@
+
+public class StillUsingVariables 
+{
+	public static void main(String[] args)
+	{
+	String class1, class2, class3, class4;
+	String teacher1, teacher2, teacher3, teacher4;
+	
+	class1 = "Spanish 2";
+	class2 = "ExplorCompSci";
+	class3 = "AP Calc AB";
+	class4 = "English 9";
+	
+	teacher1 = "Paz";
+	teacher2 = "Mauro";
+	teacher3 = "Knutsson";
+	teacher4 = "Wilson";
+	
+
+	System.out.println("|Per. 1|" + class1 + "|" + teacher1 + "|");
+	System.out.println("|Per. 2|" + class1 + "|" + teacher2 + "|");
+	System.out.println("|Per. 3|" + class1 + "|" + teacher3 + "|");
+	System.out.println("|Per. 4|" + class1 + "|" + teacher4 + "|");
+	
+		
+		
+	}
+}
